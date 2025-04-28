f8fa79bb25afe4f29a2d5657c592ba6437caadda
Natan Trennepohl <natantrennepohl@gmail.com>
2025-04-28 11:58:37 -0300

    removing buckets

--- index.ts
+++ index.ts
@@ -1,20 +1,5 @@
 import * as aws from "@pulumi/aws";
 
-const firstBucket = new aws.s3.BucketV2("primeiro-bucket", {
-  bucket: 'primeiro-bucket-pos',
-  tags: {
-      IAC: "true",
-  },
-});
-
-const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
-  bucket: 'segundo-bucket-pos',
-  tags: {
-      IAC: "true",
-  },
-});
-
-
 const ecr = new aws.ecr.Repository("teste-pulumi", {
   name: "teste-pulumi",
   imageScanningConfiguration: {
@@ -25,14 +10,6 @@ const ecr = new aws.ecr.Repository("teste-pulumi", {
     IAC: "true"
   }
 });
-
-export const firstBucketName = firstBucket.id;
-export const firstBucketRegion = firstBucket.region;
-export const firstBucketArn = firstBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
